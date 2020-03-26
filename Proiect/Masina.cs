ab6532b5844af182332e7636b76a3ff5d6af8ee6
Onofrei-Constantin <62692055+Onofrei-Constantin@users.noreply.github.com>
2020-03-26 11:44:12 +0200

    v0.2

---
+++ Proiect/Masina.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect
+{
+    public class Masina
+    {
+        string marca;
+        string model;
+        string culoare;
+        double pret;
+        int anFabricare;
+
+        public Masina()
+        {
+            marca = string.Empty;
+            culoare = string.Empty;
+            model = string.Empty;
+            pret = 0;
+            anFabricare = 0;
+        }
+        public Masina(string _marca, string _model, string _culoare, double _pret, int _anFabricare)
+        {
+            marca = _marca;
+            model = _model;
+            culoare = _culoare;
+            pret = _pret;
+            anFabricare = _anFabricare;
+        }
+        public string Info()
+        {
+            return string.Format("Masina este {0}, modelul {1}, are culoarea {2}, pretul de {3} euro, din anul {4}.",marca,model,culoare,pret,anFabricare);
+        }
+    }
+}
+
---
+++ Proiect/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new Masina();
+            string s = m.Info();
+            Console.WriteLine(s);
+
+            Masina m1 = new Masina("Audi", "A4", "negru", 4500, 2007);
+            string s1 = m1.Info();
+            Console.WriteLine(s1);
+            Console.ReadKey();
+        }
+    }
+}
